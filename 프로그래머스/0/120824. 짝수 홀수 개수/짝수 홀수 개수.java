86d70370cd03f587532eaaf9ef8cadf8db3f9107
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-06-05 11:19:44 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int odd = 0;
+        int even = 0;
+
+        for(int i=0;i<num_list.length;i++){
+            if(num_list[i] % 2==0){
+                even +=1;
+            }else{
+                odd +=1;
+            }
+        }
+        return new int[]{even,odd};
+    }
+}
