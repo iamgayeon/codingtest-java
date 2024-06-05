87f70fcd62798a80e154c08a51555793f0ddd8d2
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-06-05 11:15:12 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        while(true){
+            if( n> 0){
+                answer = answer + (n%10);
+                n = n/10;
+
+            }else
+                break;
+        }
+        return answer;
+    }
+}
