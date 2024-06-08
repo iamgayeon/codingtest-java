ccb27f660c37c7dfc7ab18625f167c3e1aaaa68b
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-06-09 00:24:07 +0900

    [Bronze I] Title: 평균, Time: 256 ms, Memory: 18804 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        double sum = 0.0;
+        int tmp = 0;
+
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int max = 0;
+
+
+        for(int i=0;i<n;i++){
+           if(arr[i] > max){
+              max = arr[i];
+           }
+        }
+
+
+        for(int i=0;i<n;i++){
+            sum +=arr[i];
+        }
+
+
+        sum = sum  * 100 / n / max;
+
+
+        System.out.print(sum);
+
+    }
+}
