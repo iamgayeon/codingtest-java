dd44f95a14b75e11ea643dfe08048871a4c55643
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-06-20 10:28:12 +0900

    [level 0] Title: 7의 개수, Time: 0.04 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        for(int i=0;i<array.length;i++) {
+            String s = Integer.toString(array[i]);
+            for(int j=0;j<s.length();j++){
+                if(s.charAt(j)== '7'){
+                    answer +=1;
+                }
+            }
+            
+        }
+            
+        return answer;
+    }
+}
+         
+
