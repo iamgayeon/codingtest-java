8b90a66dd5214f55bb4c92459c854ddaaa387cbe
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-07-01 22:46:30 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.33 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -1,61 +1,20 @@
-// // array	            result
-// // [1, 2, 3, 3, 3, 4]   3
-// import java.util.*;
-
-// class Solution {
-//     public int solution(int[] array) {
-//         Arrays.sort(array);
-//         int count = 0;
-//         int precount = 0;
-//         int answer = 0;
-//         if(array.length == 1){
-//             return array[0];
-//         }
-//         for(int i=0; i<array.length-1; i++){
-//             if(array[i] == array[i+1]){
-//                 answer = array[i+1];
-//                 count +=1;
-//             } else{
-//                 answer = array[i];
-//                 precount = count;
-//                 count = 0;
-//             }
-//         }
-//         if(count == precount){
-//             answer = -1;
-//         }
-        
-//         return answer;
-//     }
-// }
-
-import java.util.Arrays;
 class Solution {
     public int solution(int[] array) {
-        Arrays.sort(array);
-        int count = 1;
-        int maxcount = 1;
-        int answer = array[0];
-        boolean iscount = false;
-        
-        for(int i=1; i<array.length; i++){
-            if(array[i] == array[i-1]){
-                count ++;
-            }else{
-                count =1;
-            }
-            if(count > maxcount){
-                maxcount = count;
-                answer = array[i];
-                iscount = false;
+        int answer = 0;
+        int prev = -1;
+        int prevcount = -1;
+        int count = -1;
+        for(int i=1; i<array.length;i++){
+            if(prev!=array[i]){
+                if(prevcount < count){
+                    prevcount = count;
+                    prev = array[i];
+                    count ++;
+                }
                 
-            }else if(count == maxcount){
-                iscount = true;
             }
         }
-        return iscount ? -1 : answer;
-        }
+        answer = prevcount;
+        return answer;
+    }
 }
-        
-
-
