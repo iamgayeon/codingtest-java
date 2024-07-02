66bb53ab9b822719145579f1cf890026fa9adacc
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-07-02 18:41:33 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.02 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -1,19 +1,20 @@
 class Solution {
     public int solution(int[][] lines) {
-        // 1. arr 배열 및 변수 초기화
-        int[] arr = new int[200];
         int answer = 0;
+        int cnt[]=new int[200]; // 각 인덱스에 100을 더하면 200인 인덱스가 나오므로
         
-        // 2. lines 정보를 arr 배열에 적용
-        for(int i = 0; i < lines.length; i++)
-            for(int j = lines[i][0] + 100; j < lines[i][1] + 100; j++)
-                arr[j]++;
+        for(int i=0;i<3;i++){
+            for(int j=lines[i][0];j<lines[i][1];j++){
+                cnt[j+100]++; // 좌표가 음수인 값을 인덱스에 넣기 위해 100 더하기
+            }
+        }
         
-        // 3. arr 배열에서 겹친 부분 세기
-        for(int i = 0; i < 200; i++)
-            if(arr[i] > 1)
-                answer++;
+        for(int i=0;i<200;i++){
+            if(cnt[i] > 1)
+                answer+=1;
+        }
+        
         
         return answer;
     }
-  }
+}
