a4ad4c168c921c7a6cba64a79ccfee5dfdf9d1c5
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-08-01 11:02:58 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,20 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
-        int sum = brown + yellow;
-        for(int i = 3; i < sum; i++){
-            if (sum % i == 0){
-                int j = sum / i;
-                if( (i-2) * (j-2) == yellow){
-                    answer[0] = j;
-                    answer[1] = i;
-                    break;
-                }
-            }
-        }
-        
+        int[] answer = {};
         return answer;
     }
 }
