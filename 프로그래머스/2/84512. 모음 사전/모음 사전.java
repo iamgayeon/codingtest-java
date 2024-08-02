528958a8ce20c93c6c5f165bb77f34082192b57c
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-08-02 11:51:12 +0900

    [level 2] Title: 모음 사전, Time: 6.73 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,27 +1,6 @@
-import java.util.*;
-
 class Solution {
-    static String[] words = {"A", "E", "I", "O", "U"};
-    static List <String> list = new ArrayList<>();
-                    
     public int solution(String word) {
-        list = new ArrayList<>();
-        dfs("", 0);
         int answer = 0;
-        for(int i=0; i<list.size(); i++){
-            if(list.get(i).equals(word)){
-                answer = i;
-                break;
-            }
-        }
         return answer;
-    }
-    
-    public void dfs(String str, int len){
-        list.add(str);
-        if(len == 5) return;
-        for(int i=0; i<5; i++){
-            dfs(str + words[i], len+1);
-        }
     }
 }
