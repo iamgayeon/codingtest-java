99e8ce0e69201eb579698eb2e07e0a5fa618a020
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-08-06 18:39:23 +0900

    [level 3] Title: 단어 변환, Time: 0.56 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,45 +1,52 @@
 import java.util.*;
 
 class Solution {
+    boolean[] visited; // 방문한 단어(노드)
+    
     public int solution(String begin, String target, String[] words) {
+        // 필요한 변수들 정의
+        visited = new boolean[words.length]; // visited 초기화
+        Queue<String> queue = new ArrayDeque<>(); // 큐
         int answer = 0;
-        Queue<WordState> queue = new ArrayDeque<>();
-        boolean[] visited = new boolean[words.length];
         
-        queue.add(new WordState(0, begin));
         
-        while(!queue.isEmpty()) {
-            WordState cur = queue.remove();
-            if(cur.word.equals(target)){
-                return cur.cnt;
-            }
-            
-            for(int i=0; i<words.length; i++){
-                if(!visited[i] && getDiffCount(cur.word, words[i]) == 1){  visited[i] = true;
-                                                                          queue.add(new WordState(cur.cnt + 1, words[i]));
-                    
+        // bfs
+        queue.add(begin);
+        
+        while (!queue.isEmpty()) { // 큐가 빌 때까지 반복
+            for (int i=0; i<queue.size(); i++) {
+                String cur = queue.poll(); // 현재 단어 추출
+                
+                // 현재 단어가 목표 단어면 끝나고 지금이 최소 answer
+                if (cur.equals(target)) {
+                    return answer;
+                }
+                
+                for (int j=0; j<words.length; j++) {
+                // 모든 단어에 대해서 반복, 방문하지 않았고 바꿀 수 있으면 방문 처리 & 큐에 넣어주기
+                    if (visited[j] == false && canChange(cur, words[j])) {
+                        visited[j] = true;
+                        queue.add(words[j]);
+                    }
                 }
             }
+            answer ++; // 너비 하나 탐색 완료 후 answer ++
         }
-        
-        return 0;
+        return 0; // 목표 단어로 변환할 수 없는 경우 !!!!
     }
     
-    int getDiffCount(String word, String target){
-        int diffCount = 0;
-        for(int i=0; i<word.length(); i++){
-            if(word.charAt(i) != target.charAt(i)) diffCount++;
-        }
-        return diffCount;
-    }
     
-    class WordState {
-        int cnt; 
-        String word;
+    // 바꿀 수 있는지 확인하는 메소드 
+    public boolean canChange(String a, String b) {
+        int diffCnt = 0;
         
-        WordState(int cnt, String word){
-            this.cnt = cnt;
-            this.word = word;
+        for (int i=0; i<a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                diffCnt ++;
+            }
         }
+        
+        if (diffCnt == 1) return true;
+        else return false;
     }
 }
