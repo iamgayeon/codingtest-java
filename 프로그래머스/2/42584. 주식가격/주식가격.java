e0a05ccdee380c05372060f73971f22171a4fee5
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-08-07 18:55:40 +0900

    [level 2] Title: 주식가격, Time: 29.89 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,27 +2,24 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
+        Deque<Integer> stack = new ArrayDeque<>();
         int[] answer = new int[prices.length];
-        Stack<Integer> stack = new Stack<>();
         for(int i=0; i<prices.length; i++){
             while(!stack.isEmpty()){
                 int j = stack.peek();
-                
                 if(prices[j] > prices[i]){
                     answer[j] = i - j;
                     stack.pop();
-                }
-                else break;
+                } else
+                    break;
             }
             stack.push(i);
         }
-        
         while(!stack.isEmpty()){
             int i = stack.pop();
             answer[i] = prices.length - i - 1;
+            
         }
-        
-        
         return answer;
     }
 }
