a963d6c6cffa79fce1f5ca848d8f1cadb1d6015d
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-08-09 10:27:59 +0900

    [level 2] Title: 괄호 회전하기, Time: 31.47 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,28 +1,29 @@
 import java.util.*;
 
+
 class Solution {
+    int answer;
     public int solution(String s) {
-        int answer = 0;
+        answer = 0;
         String sb = s + s;
-        for(int i=0; i<s.length(); i++){
-            if(isValid(sb.substring(i, s.length() + i)))
-                answer++;
+        for(int i =0; i< s.length(); i++){
+            if(isValid(sb.substring(i, s.length() + i))) answer++;
         }
         return answer;
     }
+    
     public boolean isValid(String s){
         Deque<Character> stack = new ArrayDeque<>();
-        
         for(char current : s.toCharArray()){
             if(current == '(' || current == '{' || current == '['){
                 stack.push(current);
-            } else {
+            }else {
                 if(stack.isEmpty()) return false;
                 
                 char target = stack.pop();
                 if((target == '(' && current != ')') ||
-                   (target == '{' && current != '}') ||
-                   (target == '[' && current != ']')) {
+                   (target == '[' && current != ']') ||
+                   (target == '{' && current != '}')){
                     return false;
                 }
             }
