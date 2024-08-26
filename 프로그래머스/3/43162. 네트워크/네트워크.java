2d1e05ef54589da21fe752a025be816d5c715b91
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-08-26 14:03:38 +0900

    [level 3] Title: 네트워크, Time: 0.24 ms, Memory: 93.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,26 @@
+class Solution {
+    static int answer;
+    boolean[] visited;
+    public int solution(int n, int[][] computers) {
+        answer = 0;
+        visited = new boolean[n];
+        for(int i=0; i<n; i++){
+            if(!visited[i]){
+                dfs(n,computers, visited, i);
+                answer++;
+            }
+        }
+        return answer;
+    }
+    
+    void dfs(int n, int[][] computers, boolean[] visited, int i){
+        visited[i] = true;
+        for(int j=0; j<n; j++){
+            if(!visited[j]){
+               if(computers[i][j] ==1){
+                    dfs(n, computers, visited, j);
+               }
+            }
+        }
+    }
+}
