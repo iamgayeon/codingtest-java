f871d510a644d4bc7544682c6055bda050fe2cd1
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-08-27 16:48:41 +0900

    [level 2] Title: 소수 찾기, Time: 6.74 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,20 +1,21 @@
 import java.util.*;
 
 class Solution {
+    Set<Integer> set = new HashSet<>();
     public int solution(String numbers) {
         
         // 소수의 개수를 셀 변수
         int answer = 0;
         
         // 소수가 저장될 Set, 중복된 숫자가 나오지 않도록 HashSet 이용
-        Set<Integer> set = new HashSet<>();
+        
         
         // 주어진 문자열 numbers에서 숫자 조합 생성
         permutation(numbers, 0, new boolean[numbers.length()], 0);
         
         // 각 숫자 조합이 소수인지 확인하여 소수라면 카운트 증가    
         for(int n : set){
-            if(isPrime(num)) answer++;
+            if(isPrime(n)) answer++;
         }
         
         
@@ -35,7 +36,7 @@ class Solution {
             if(visited[i]) continue;
             
             // 현재 숫자와 자릿수를 이용하여 새로운 값 생성
-            int value = current + (int)(numbers.charAt(i) -'0') * Math.pow(10, digit);
+            int value = current * 10 + (int)(numbers.charAt(i) -'0');
             
             // 값을 HashSet에 추가
             set.add(value);
@@ -43,6 +44,8 @@ class Solution {
             // 현재 숫자를 방문 처리
             visited[i] = true;
             permutation(numbers, value, visited, digit + 1);
+            // 방문 처리 해제 (백트래킹)
+            visited[i] = false;
         }
     }
     
