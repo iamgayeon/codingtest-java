8edc4d59da9bc9f7b2cf0b30d5ff77b7baecd76b
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-08-28 17:00:48 +0900

    [level 2] Title: 소수 찾기, Time: 7.19 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,61 +1,36 @@
 import java.util.*;
 
 class Solution {
-    Set<Integer> set = new HashSet<>();
+    Set<Integer> candidates = new HashSet<>();
     public int solution(String numbers) {
-        
-        // 소수의 개수를 셀 변수
+        permutation(numbers, 0, new boolean[numbers.length()+1], 0);
         int answer = 0;
-        
-        // 소수가 저장될 Set, 중복된 숫자가 나오지 않도록 HashSet 이용
-        
-        
-        // 주어진 문자열 numbers에서 숫자 조합 생성
-        permutation(numbers, 0, new boolean[numbers.length()], 0);
-        
-        // 각 숫자 조합이 소수인지 확인하여 소수라면 카운트 증가    
-        for(int n : set){
-            if(isPrime(n)) answer++;
+        for(int num : candidates){
+            if(isPrime(num)) answer++;
         }
-        
-        
-    
         return answer;
     }
     
-    // 주어진 문자열에서 숫자 조합을 생성하는 재귀 함수
-    // current 변수 -> 현재 생성 중인 숫자
-    // digit 변수 -> 현재 생성 중인 숫자의 자릿수
     void permutation(String numbers, int current, boolean[] visited, int digit){
-        // 모든 자릿수에 대해 숫자 조합을 생성했다면 종료
-        // ex. 1234 -> length -> 4 -> digit이 4가 되면 종료
         if(digit == numbers.length()) return;
         
-        for(int i = 0; i < numbers.length(); i++){
-            // 이미 방문한 숫자는 건너뛰기
+        for(int i=0; i<numbers.length(); i++){
             if(visited[i]) continue;
+            int newvalue = current + (int)((numbers.charAt(i) - '0') * Math.pow(10, digit));
             
-            // 현재 숫자와 자릿수를 이용하여 새로운 값 생성
-            int value = current * 10 + (int)(numbers.charAt(i) -'0');
+            candidates.add(newvalue);
             
-            // 값을 HashSet에 추가
-            set.add(value);
-            
-            // 현재 숫자를 방문 처리
             visited[i] = true;
-            permutation(numbers, value, visited, digit + 1);
-            // 방문 처리 해제 (백트래킹)
+            permutation(numbers, newvalue, visited, digit+1);
             visited[i] = false;
         }
     }
     
-    
-    
     boolean isPrime(int n){
-        for(int i=2; i<n; i++){
-            if(i%n == 0){
-                return false;
-            }
+        if(n < 2) return false;
+        
+        for(int i=2; i*i<=n; i++){
+            if(n % i == 0) return false;
         }
         return true;
     }
