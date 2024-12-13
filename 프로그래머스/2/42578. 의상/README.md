6bb325e08e9c0a13e22cf3ae182db15d2f3107e9
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-12-13 15:11:08 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
 
 ### 성능 요약
 
-메모리: 83.4 MB, 시간: 0.16 ms
+메모리: 76.3 MB, 시간: 0.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 11일 15:24:54
+2024년 12월 13일 15:11:06
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,23 +2,18 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        // 의상 종류별로 개수를 세기 위한 맵
         Map<String, Integer> map = new HashMap<>();
         
-        // 의상 배열을 순회하며 의상 종류별 개수를 셈
-        for (String[] cloth : clothes) {
-            String type = cloth[1]; // 의상 종류
-            map.put(type, map.getOrDefault(type, 0) + 1); // 맵에 의상 종류와 개수 추가
+        for(String[] cloth : clothes){
+            String type = cloth[1];
+            map.put(type, map.getOrDefault(type, 0) + 1);
         }
         
-        int answer = 1; // 결과를 저장할 변수, 조합 계산을 위해 1로 초기화
+        int answer = 1;
         
-        // 각 의상 종류별로 조합 가능한 경우의 수를 계산
-        for (int count : map.values()) {
-            answer *= (count + 1); // 각 의상 종류에서 선택하거나 선택하지 않는 경우의 수
+        for(int count : map.values()){
+            answer *= count + 1;
         }
-        
-        // 아무것도 입지 않는 경우를 제외하기 위해 1을 뺀다
-        return answer - 1;
+        return answer-1;
     }
 }
