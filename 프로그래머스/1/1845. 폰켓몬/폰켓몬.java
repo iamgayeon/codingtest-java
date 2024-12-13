a6c5557b342f43fc327d00f27d970b8da3aa01ef
iamgayeon <97969511+iamgayeon@users.noreply.github.com>
2024-12-13 13:32:14 +0900

    [level 1] Title: 폰켓몬, Time: 3.60 ms, Memory: 89.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int len = nums.length / 2;
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int num: nums){
+            set.add(num);
+        }
+        
+        int size = set.size();
+        
+        if(size >= len){
+            return len;
+        }else{
+            return size;
+        }
+    }
+}
